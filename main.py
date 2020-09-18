637928514c492e8f2ca53d3904abc02ce6f803f4
AlfyCodes <alfred.ortiz1@marist.edu>
2020-09-18 11:30:56 -0400

    adding methods to handle the grid

--- main.py
+++ main.py
@@ -19,3 +19,22 @@ ORANGE = (255, 165, 0)
 GREY = (128, 128, 128)
 TURQUOISE = (64, 224, 208)
 
+# Keeping track of the nodes in the grid (The spots on the grid)
+class Spot: 
+    def __init__(self, row, col, width, total_rows) # 
+    self.row = row 
+    self.col = col
+    self.x = row * width 
+    self.y = col * width 
+    self.color = WHITE # All cubes will be white at start
+    self.neighbors = [] 
+    self.width = width 
+    self.total_rows 
+
+# Gets the position
+def get_pos(self):
+    return self.row, self.col 
+
+# The red squares will indicate that we already looked at this position 
+def is_closed(self):
+    return self.color == RED
