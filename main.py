7ca7a97d3565fabbbffbcc0b4287399e387d08e2
AlfyCodes <alfred.ortiz1@marist.edu>
2020-09-18 11:48:51 -0400

    install autopep and fix indents

--- main.py
+++ main.py
@@ -14,76 +14,81 @@ BLUE = (0, 255, 0)
 YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-PURPLE = (128, 0 , 128)
+PURPLE = (128, 0, 128)
 ORANGE = (255, 165, 0)
 GREY = (128, 128, 128)
 TURQUOISE = (64, 224, 208)
 
 # Keeping track of the nodes in the grid (The spots on the grid)
-class Spot: 
-    def __init__(self, row, col, width, total_rows) # 
-    self.row = row 
+
+
+class Spot:
+    def __init__(self, row, col, width, total_rows)
+    self.row = row
     self.col = col
-    self.x = row * width 
-    self.y = col * width 
-    self.color = WHITE # All cubes will be white at start / Also indicates areas not yet looked at
-    self.neighbors = [] 
-    self.width = width 
-    self.total_rows 
+    self.x = row * width
+    self.y = col * width
+    # All cubes will be white at start / Also indicates areas not yet looked at
+    self.color = WHITE
+    self.neighbors = []
+    self.width = width
+    self.total_rows
 
 # Gets the position
-def get_pos(self):
-    return self.row, self.col 
+    def get_pos(self):
+        return self.row, self.col
 
-# The red squares will indicate that we already looked at this position 
-def is_closed(self):
-    return self.color == RED
+# The red squares will indicate that we already looked at this position
+
+
+    def is_closed(self):
+        return self.color == RED
 
 # Open Set
- def is_open(self):
-    return self.colo == ORANGE
+    def is_open(self):
+        return self.colo == ORANGE
 
 # Barrier
-def is_barrier(self):
-    return self.color == BLACK
+    def is_barrier(self):
+        return self.color == BLACK
 
 # Starting Location
-def is_start(self):
-    return self.color == GREEN
+    def is_start(self):
+        return self.color == GREEN
 
 # Ending Location
-def is_end(self):
-    return self.color == PURPLE
+    def is_end(self):
+        return self.color == PURPLE
 
 # Resets the Grid
-def reset(self):
-    self.color == WHITE
+    def reset(self):
+        self.color == WHITE
 
 # Change the colors
-def make_closed(self):
-    self.color = RED
+    def make_closed(self):
+        self.color = RED
 
-def make_open(self):
-    self.color = GREEN
+    def make_open(self):
+        self.color = GREEN
 
-def make_barrier(self):
-    self.color = BLACK
+    def make_barrier(self):
+        self.color = BLACK
 
-def make_end(self):
-    self.color = PURPLE
+    def make_end(self):
+        self.color = PURPLE
 
-def make_path(self):
-    self.color = TURQUOISE
+    def make_path(self):
+        self.color = TURQUOISE
 
-def draw(self, win):
-    pygame.draw.rect(win, self.color, ())
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, ())
 
-def update_neighbors(self, grid):
-    pass
+    def update_neighbors(self, grid):
+        pass
 
 # Less than that handles Spot comparisons 
-def __lt__(self, other):
-    return False
+    def __lt__(self, other):
+        return False
 
 # Heuristic Function / Figure the distance between point 1 and point 2 using Manhattan Distance
 def h(p1, p2):
@@ -91,7 +96,6 @@ def h(p1, p2):
     x2, y2 = p2
     # Use distance formula 
     return abs(x1 - x2) + abs(y1 - y2)
-
 
 
     
